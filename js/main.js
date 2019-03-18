128e5e106565dc9c2cfbbe37eeb2205d4fd747c2
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-03-18 19:44:36 +0400

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
 let HouMin = 0;
-function clock()
-{
+
+const clock = () => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    
 
-    if(hours < 10)
+    if (hours < 10)
         hours = "0" + hours;
-    if(minutes < 10)
+    if (minutes < 10)
         minutes = "0" + minutes;
-    
 
-    HouMin= hours + ":" + minutes;
-
+    HouMin = hours + ":" + minutes;
     document.getElementById("clok").innerHTML = HouMin;
-
     Timer = setTimeout("clock()", 1000);
 }
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-            };
-  })();
-  (function clock(){ 
-      (function loop(){
-          requestAnimFrame(loop);
-          draw();
-      })();
-      function draw(){
-          var now = new Date(),//now
-              then = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0,0,0),
-              diffInMil = (now.getTime() - then.getTime()),
-              h = (diffInMil/(1000*60*60)),
-              m = (h*60),
-              s = (m*60);
-          document.getElementById("sun").style.webkitTransform = "rotate(" + (s * 6) + "deg)";
-      } 
-  })();
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+(function clock() {
+    (function loop() {
+        requestAnimFrame(loop);
+        draw();
+    })();
+
+    function draw() {
+        var now = new Date(), //now
+            then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0),
+            diffInMil = (now.getTime() - then.getTime()),
+            h = (diffInMil / (1000 * 60 * 60)),
+            m = (h * 60),
+            s = (m * 60);
+        document.getElementById("sun").style.webkitTransform = "rotate(" + (s * 6) + "deg)";
+    }
+})();
