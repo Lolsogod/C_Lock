ef3d8e060462bc55ee1a71dd3a898334d7d30b50
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-03-18 18:54:07 +0400

    smoother animation

--- js/main.js
+++ js/main.js
@@ -4,17 +4,40 @@ function clock()
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    
 
     if(hours < 10)
         hours = "0" + hours;
     if(minutes < 10)
         minutes = "0" + minutes;
-    seconds = seconds*6;
+    
 
     HouMin= hours + ":" + minutes;
 
     document.getElementById("clok").innerHTML = HouMin;
-    document.getElementById("sun").style.transform = `rotate(${seconds}deg)`
+
     Timer = setTimeout("clock()", 1000);
 }
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+  (function clock(){ 
+      (function loop(){
+          requestAnimFrame(loop);
+          draw();
+      })();
+      function draw(){
+          var now = new Date(),//now
+              then = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0,0,0),
+              diffInMil = (now.getTime() - then.getTime()),
+              h = (diffInMil/(1000*60*60)),
+              m = (h*60),
+              s = (m*60);
+          document.getElementById("sun").style.webkitTransform = "rotate(" + (s * 6) + "deg)";
+      } 
+  })();
