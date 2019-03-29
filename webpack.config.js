59e11be595b793be90ad73477e93da134eaccf64
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-03-29 21:17:46 +0400

    IE basic support

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,30 @@ let conf = {
     entry: "./src/js/main.js",
     output: {
         path: path.resolve(__dirname, "./js"),
-        filename: "main.js"
-    }
+        filename: "main.js",
+        publicPath: "/js"
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    devtool: "eval-sourcemap"
 }
 
 // eslint-disable-next-line no-undef
-module.exports =conf;
+module.exports = (enviroment, options) => {
+    let production = options.mode === "production";
+    conf.devtool =  production ? 'source-map' : 'eval-sourcemap';
+    return conf;
+}
